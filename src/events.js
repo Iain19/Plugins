2fa532d0ed6711cde873ad132f458d80a9aaac5f
Yevhen Pavliuk <yevhenp@wix.com>
2014-10-12 00:39:02 +0300

    Use this instead of the argument in events plugin
    That is done to make the code testable.
    
    That also convinient because you can wrap all your plugins in a closure
    where this is set to the object where the plugins expose.

--- src/events.js
+++ src/events.js
@@ -2,21 +2,16 @@
  * Cross-browser functions for work with Events
  *
  * @version 0.0.1
- *
- * @param {object} extendableObject - extend object with new methods. If not specified, 'window' will be used.
  */
-(function(extendableObject) {
+(function() {
   'use strict';
-  if (!(extendableObject instanceof Object)) {
-    extendableObject = window;
-  }
   /**
    * @param {HTMLElement} elem - element on which we hang event handler
    * @param {string} event - event we want to handle on
    * @param {function} handler - event handler
    * @param {boolean} elem - flag that define a using of capturing or bubbling stage of event
    */
-  extendableObject.addEvent = function(elem, event, handler, useCapture) {
+  this.addEvent = function(elem, event, handler, useCapture) {
     if (useCapture === undefined) {
       useCapture = false;
     }
@@ -32,7 +27,7 @@
    * @param {function} handler - event handler we want to remove
    * @param {boolean} elem - flag stage phase
    */
-  extendableObject.removeEvent = function(elem, event, handler, useCapture) {
+  this.removeEvent = function(elem, event, handler, useCapture) {
     if (useCapture === undefined) {
       useCapture = false;
     }
