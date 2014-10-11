3372e55ef9c72e2e6822817f9d013a1409637e5d
Yevhen Pavliuk <yevhenp@wix.com>
2014-10-12 00:45:43 +0300

    Remove useCapture argument from events plugin
    Because it cannot be applied in IE8, and because of that most likely it
    is not used at all in campaign development.

--- src/events.js
+++ src/events.js
@@ -9,14 +9,10 @@
    * @param {HTMLElement} elem - element on which we hang event handler
    * @param {string} event - event we want to handle on
    * @param {function} handler - event handler
-   * @param {boolean} elem - flag that define a using of capturing or bubbling stage of event
    */
-  this.addEvent = function(elem, event, handler, useCapture) {
-    if (useCapture === undefined) {
-      useCapture = false;
-    }
+  this.addEvent = function(elem, event, handler) {
     if (typeof elem.addEventListener === 'function') {
-      elem.addEventListener(event, handler, useCapture);
+      elem.addEventListener(event, handler);
     } else {
       elem.attachEvent('on' + event, handler);
     }
@@ -25,14 +21,10 @@
    * @param {HTMLElement} elem - element from which we remove event handler
    * @param {string} event - event we want to remove a handle from
    * @param {function} handler - event handler we want to remove
-   * @param {boolean} elem - flag stage phase
    */
-  this.removeEvent = function(elem, event, handler, useCapture) {
-    if (useCapture === undefined) {
-      useCapture = false;
-    }
+  this.removeEvent = function(elem, event, handler) {
     if (typeof elem.removeEventListener === 'function') {
-      elem.removeEventListener(event, handler, useCapture);
+      elem.removeEventListener(event, handler);
     } else {
       elem.detachEvent('on' + event, handler);
     }
